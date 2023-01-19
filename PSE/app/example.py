e9e16a0a87656ce9f44f1aa584b4fcac7ed054f7
Jmin <90593474+Jmin0613@users.noreply.github.com>
2023-01-20 05:13:30 +0900

    Delete PSE directory

--- PSE/app/example.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Form
-
-app = FastAPI()
-
-
-@app.post("/login")
-async def login(username: str = Form(default={})):
-    print(username)
-    return {"username": username}
